c9c98ffeb7b2db7cd89aa5c46439039eb636faf2
Alyona Rogovaya <alyona@yelk.com.ua>
2025-10-20 20:02:48 +0200

    Solved the task 75 - sort colors

---
+++ 75-Sort-Colors/index.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} nums
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var sortColors = function (nums) {
+  for (let j = nums.length - 1; j > 0; j--) {
+    for (let i = 0; i < j; i++) {
+      if (nums[i] > nums[i + 1]) {
+        const temp = nums[i];
+        nums[i] = nums[i + 1];
+        nums[i + 1] = temp;
+      }
+    }
+  }
+};
