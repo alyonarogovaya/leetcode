aae0af2ca5e86e3c03d90445c9b008717d2e632a
Alyona Rogovaya <alyona@yelk.com.ua>
2025-10-20 19:11:21 +0200

    Add one more solution for the valid parentheses task

--- 20-Valid-Parentheses/index.js
+++ 20-Valid-Parentheses/index.js
@@ -6,19 +6,19 @@
 const isValid = function (s) {
   let stack = [];
   for (let ch of s) {
-    if (ch == "(" || ch == "[" || ch == "{") {
+    if (ch == '(' || ch == '[' || ch == '{') {
       stack.push(ch);
     } else {
       let tmp = stack.pop();
       switch (ch) {
-        case ")":
-          if (tmp != "(") return false;
+        case ')':
+          if (tmp != '(') return false;
           break;
-        case "]":
-          if (tmp != "[") return false;
+        case ']':
+          if (tmp != '[') return false;
           break;
-        case "}":
-          if (tmp != "{") return false;
+        case '}':
+          if (tmp != '{') return false;
           break;
         default:
           return false;
@@ -26,4 +26,27 @@ const isValid = function (s) {
     }
   }
   return stack.length == 0;
+};
+
+const isValid2 = function (string) {
+  const brackets = new Map([
+    [')', '('],
+    [']', '['],
+    ['}', '{'],
+  ]);
+
+  const stack = [];
+
+  for (const char of string) {
+    if (brackets.has(char)) {
+      const top = stack.length ? stack.pop() : null;
+      if (top !== brackets.get(char)) {
+        return false;
+      }
+    } else {
+      stack.push(char);
+    }
+  }
+
+  return stack.length === 0;
 };
