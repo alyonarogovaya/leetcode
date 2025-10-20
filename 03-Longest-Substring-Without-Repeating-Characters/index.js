1800dc2a3d12def9e73a19341f1116b49ab4f8ba
Alyona Rogovaya <alyona@yelk.com.ua>
2025-10-20 18:47:43 +0200

    Add a solution for the length of the longest substring task

---
+++ 03-Longest-Substring-Without-Repeating-Characters/index.js
@@ -0,0 +1,17 @@
+function lengthOfLongestSubstring(s) {
+  let start = 0;
+  let maxLength = 0;
+  const seen = new Map();
+
+  for (let end = 0; end < s.length; end++) {
+    const char = s[end];
+
+    if (seen.has(char) && seen.get(char) >= start) {
+      start = seen.get(char) + 1;
+    }
+    seen.set(char, end);
+    maxLength = Math.max(maxLength, end - start + 1);
+  }
+
+  return maxLength;
+}
