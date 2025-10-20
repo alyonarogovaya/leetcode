4b30472e2e21aea81ab8e34a8baf65abda7c10b8
Alyona Rogovaya <alyona@yelk.com.ua>
2025-10-20 18:23:34 +0200

    Add a solution for the 01-two-sum task

---
+++ 01-two-sum/index.js
@@ -0,0 +1,12 @@
+const twoSum = function (nums, target) {
+  const map = new Map();
+  for (let i = 0; i < nums.length; i++) {
+    const result = target - nums[i];
+
+    if (map.has(result)) {
+      return [map.get(result), i];
+    }
+
+    map.set(nums[i], i);
+  }
+};
